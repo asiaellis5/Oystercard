465dd0ef3617b50b65ab131637fee28907c8b165
shaunlsy <shaunlowshuennyuh@hotmail.com>
2019-12-18 17:16:25 +0000

    Oystercard Challenge 15.1

---
+++ spec/journey_log_spec.rb
@@ -0,0 +1,9 @@
+require 'journey_log'
+
+describe JourneyLog do
+  describe "initialization" do
+    it 'initializes with a new journey class' do
+      expect(subject.journey_class).to eq Journey.new
+    end
+  end
+end
